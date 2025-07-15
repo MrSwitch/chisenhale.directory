f1499d7535b59087317ef418135ff69a50cd8839
Andrew Dodson <andrew@5app.com>
2025-07-15 21:39:59 +0100

    fix(postcss): apply postcss to serve unnested content 😭, for Tamara 💫

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,15 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import postcss from 'postcss';
+import postcssImport from 'postcss-import';
+import postcssMediaMinmax from 'postcss-media-minmax';
+import postcssNested from 'postcss-nested';
 
 export default function (eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		widths: ["1800"],
     formats: ["webp"],
   });
-  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addTemplateFormats('css');
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.ignores.add("README.md");
@@ -13,4 +17,25 @@ export default function (eleventyConfig) {
   eleventyConfig.addShortcode("obfusicate", (content) => {
     return `<script>document.write('${content.split('').join(`'+'`)}');</script>`;
   });
+
+  eleventyConfig.addExtension('css', {
+    outputFileExtension: 'css',
+    async compile(content, path) {
+      // Processing only process style.css
+      if (path !== './style.css') {
+        return;
+      }
+      return async () => {
+        let output = await postcss([
+          postcssImport,
+          postcssNested,
+          postcssMediaMinmax,
+        ]).process(content, {
+          from: path,
+        });
+
+        return output.css;
+      }
+    }
+});
 }
